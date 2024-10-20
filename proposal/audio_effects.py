0d360db2b6c1a4e6ae6055e3b2de498ad92b4358
IvonLuiz <ivonluizgarcia@gmail.com>
2024-10-19 22:16:04 -0300

    AudioEffects class for putting multiple effects in single audio at the same time using sox

--- proposal/audio_effects.py
+++ proposal/audio_effects.py
@@ -1,42 +1,74 @@
-import numpy as np
 import sox
 
-def AudioEffects():
+
+class AudioEffects():
     def __init__(self, audio_path):
         self.audio_path = audio_path
         self.audio_name = audio_path[:-4]
-    
-    def add_effects(self, reverb=False, delay=False, tremolo=False):
-        self.tfm = sox.Transformer()        
 
-        if (reverb==True):
-            self.add_reverb()
+    def add_effects(self,
+                    reverb_specs=None,
+                    delay_specs=None,
+                    tremolo_specs=None,
+                    flanger_specs=None,
+                    pitch_shift=None):
         
-        if (tremolo==True):
-            self.add_tremolo()
+        self.tfm = sox.Transformer()        
+        effects = []
 
-        if (delay==True):
-            self.add_delay()        
+        if (reverb_specs != None):
+            self.add_reverb(reverb_specs)
+            effects.append("reverb")
+        
+        if (tremolo_specs != None):
+            self.add_tremolo(tremolo_specs)
+            effects.append("tremolo")
 
-
+        if (delay_specs != None):
+            self.add_delay(delay_specs)        
+            effects.append("delay")
+        
+        if (flanger_specs != None):
+            self.add_flanger(flanger_specs)        
+            effects.append("flanger")
+        
+        if (pitch_shift != None):
+            self.add_pitch(pitch_shift)
+            effects.append("pitchshift")
+        
         status = self.tfm.build_file(
             input_filepath=self.audio_path,
-            output_filepath=self.audio_name + '_rev_delay_out.wav'
+            output_filepath=self.audio_name + "_" + "_".join(effects) + '.wav'
         )
         
-        print(status)
+        print("Effects",  effects, "applied", self.audio_name)
     
-    def add_reverb(self):
-        self.tfm.reverb(reverberance=50,
-                high_freq_damping=50,
-                room_scale=100,
-                stereo_depth=100,
-                pre_delay=0,
-                wet_gain=0,
-                wet_only=False)
+
+    def add_reverb(self, specs):
+        self.tfm.reverb(reverberance=specs[0],
+                        high_freq_damping=specs[1],
+                        room_scale=specs[2],
+                        stereo_depth=specs[3],
+                        pre_delay=0,
+                        wet_gain=0,
+                        wet_only=False)
         
-    def add_delay(self):
-        None
-        
-    def add_tremolo(self):
-        None
+    def add_delay(self, specs):
+        self.tfm.echos(gain_in=specs[0],
+                       gain_out=specs[1],
+                       n_echos=len(specs[3]),
+                       delays=specs[2],
+                       decays=specs[3])
+    
+    def add_tremolo(self, specs):
+        self.tfm.tremolo(speed=specs[0], depth=specs[1])
+    
+    def add_pitch(self, pitch):
+        self.tfm.pitch(pitch)
+
+    def add_flanger(self, specs):
+        self.tfm.flanger(delay=specs[0],
+                         depth=specs[1],
+                         regen=specs[2],
+                         width=specs[3],
+                         speed=specs[4])
