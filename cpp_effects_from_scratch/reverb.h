a6dbb6e804af7aa14b3c072133f7c13d80447f97
IvonLuiz <ivonluizgarcia@gmail.com>
2024-12-29 23:28:56 -0300

    .h functions

--- cpp_effects_from_scratch/reverb.h
+++ cpp_effects_from_scratch/reverb.h
@@ -4,6 +4,11 @@
 #include <vector>
 #include <cstdint>
 
-std::vector<int16_t> combFilter(std::vector<int16_t> inputSamples, double delayMs, double decayGain, float sampleRate);
+// std::vector<int16_t> combFilter(std::vector<int16_t> inputSamples, double delayMs, double decayGain, float sampleRate);
+
+std::vector<int16_t> combFilter(const std::vector<int16_t>& inputSamples, double delayMs, double decayGain, float sampleRate);
+std::vector<int16_t> allPassFilter(const std::vector<int16_t>& inputSamples, double delayMs, double decayGain, float sampleRate);
+std::vector<int16_t> schroederReverb(const std::vector<int16_t>& inputSamples, float sampleRate);
+std::vector<int16_t> mixDryWet(const std::vector<int16_t>& dry, const std::vector<int16_t>& wet, float wetLevel);
 
 #endif // COMB_FILTER_H
