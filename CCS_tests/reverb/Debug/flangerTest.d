247902668eb5277b61a3935830934f2b657c2872
IvonLuiz <ivonluizgarcia@gmail.com>
2025-01-06 18:31:34 -0300

    projects settings

---
+++ CCS_tests/reverb/Debug/flangerTest.d
@@ -0,0 +1,89 @@
+# FIXED
+
+flangerTest.obj: ../flangerTest.c
+flangerTest.obj: C:/ti/ezdsp5502_v1/include/ezdsp5502.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/_csl.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_chiphal.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_std.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/tistdtypes.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_stdhal.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_chipdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_icachdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_irqdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_datdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_dmadat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_dmadatB.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_emifdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_gpiodat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502dat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_plldat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_mcbspdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_pwrdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_timdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_gptdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_bctdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_errdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_mmcdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_wdtimdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_rtcdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_time.h
+flangerTest.obj: C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/linkage.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_i2cdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_uartdat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_csldat.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_cslhal.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_irq.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/_csl.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_irqhal.h
+flangerTest.obj: C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_err.h
+flangerTest.obj: C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdlib.h
+flangerTest.obj: C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdio.h
+flangerTest.obj: C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdarg.h
+flangerTest.obj: ../flanger.h
+
+../flangerTest.c: 
+C:/ti/ezdsp5502_v1/include/ezdsp5502.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/_csl.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_chiphal.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_std.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/tistdtypes.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_stdhal.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_chipdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_icachdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_irqdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_datdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_dmadat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_dmadatB.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_emifdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_gpiodat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502dat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_plldat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_mcbspdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_pwrdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_timdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_gptdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_bctdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_errdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_mmcdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_wdtimdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_rtcdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_time.h: 
+C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/linkage.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_i2cdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_uartdat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_csldat.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_cslhal.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_irq.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/_csl.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_irqhal.h: 
+C:/ti/ezdsp5502_v1/C55xxCSL/include/csl_err.h: 
+C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdlib.h: 
+C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/c5500_4.4.1/include/stdarg.h: 
+../flanger.h: 
