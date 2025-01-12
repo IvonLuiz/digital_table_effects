6525612a2ccd6958e66f90c8d5ed8149451388e7
IvonLuiz <ivonluizgarcia@gmail.com>
2025-01-12 16:54:23 -0300

    Fourier transform

--- cpp_effects_from_scratch/src/effects/pitchShifter.cpp
+++ cpp_effects_from_scratch/src/effects/pitchShifter.cpp
@@ -17,3 +17,31 @@ void applyWindow(std::vector<Sample>& buffer, const std::vector<Sample>& window)
     std::transform(buffer.begin(), buffer.end(), window.begin(), buffer.begin(), std::multiplies<Sample>());
 }
 
+
+void fft(const std::vector<Sample>& input, std::vector<Complex>& output) {
+    int n = input.size();
+    output.resize(n);
+
+    for (int k = 0; k < n; ++k) {
+        output[k] = {0, 0};
+        for (int t = 0; t < n; ++t) {
+            double angle = 2 * M_PI * t * k / n;
+            output[k] += input[t] * Complex(std::cos(angle), -std::sin(angle));
+        }
+    }
+}
+
+
+void ifft(const std::vector<Complex>& input, std::vector<Sample>& output) {
+    int n = input.size();
+    output.resize(n);
+    for (int t = 0; t < n; ++t) {
+        Complex sum = {0, 0};
+        for (int k = 0; k < n; ++k) {
+            double angle = 2 * M_PI * t * k / n;
+            sum += input[k] * Complex(std::cos(angle), std::sin(angle));
+        }
+        output[t] = sum.real() / n;
+    }
+}
+
