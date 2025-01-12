4fe63ab2b0b1e902114e94870ba31387bdaf3c03
IvonLuiz <ivonluizgarcia@gmail.com>
2025-01-12 18:56:29 -0300

    testing pitch shift

--- cpp_effects_from_scratch/src/main.cpp
+++ cpp_effects_from_scratch/src/main.cpp
@@ -2,16 +2,18 @@
 #include <vector>
 #include <iostream>
 #include "effects/reverb.h"
+#include "effects/pitchShifter.h"
 #include "audio/handleAudio.h"
 
 int main() {
     const char* filename = "src/original.wav";
-    const char* outputFilename = "src/audio_reverb.wav";
+    const char* outputFilename = "src/audio_pitch.wav";
     float wetLevel = 0.5f; // 50% wet signal
+    float pitchShift = 1.5f; // Shift pitch by 1.5 times
 
     std::ifstream inputFile(filename, std::ios::binary);
     if (!inputFile) {
-        std::cerr << "Error: Could not open file " << filename << std::endl;
+        std::cerr << "Error: Could not open file " << filename << "\n";
         return 1;
     }
 
@@ -20,21 +22,21 @@ int main() {
     inputFile.read(reinterpret_cast<char*>(&header), sizeof(WavHeader));
 
     if (std::string(header.riff, 4) != "RIFF" || std::string(header.wave, 4) != "WAVE") {
-        std::cerr << "Error: Not a valid WAV file." << std::endl;
+        std::cerr << "Error: Not a valid WAV file.\n";
         return 1;
     }
 
-    std::cout << "Sample Rate: " << header.sampleRate << " Hz" << std::endl;
-    std::cout << "Channels: " << header.numChannels << std::endl;
-    std::cout << "Bits per Sample: " << header.bitsPerSample << std::endl;
-    std::cout << "Data Size: " << header.dataSize << " bytes" << std::endl;
+    std::cout << "Sample Rate: " << header.sampleRate << " Hz" << "\n";
+    std::cout << "Channels: " << header.numChannels << "\n";
+    std::cout << "Bits per Sample: " << header.bitsPerSample << "\n";
+    std::cout << "Data Size: " << header.dataSize << " bytes" << "\n";
 
     // Reads audio dada to vector of bytes
     std::vector<uint8_t> audioData(header.dataSize);
     inputFile.read(reinterpret_cast<char*>(audioData.data()), header.dataSize);
 
     if (!inputFile) {
-        std::cerr << "Error: Could not read audio data." << std::endl;
+        std::cerr << "Error: Could not read audio data. \n";
         return 1;
     }
 
@@ -42,13 +44,21 @@ int main() {
 
     std::vector<float> floatSamples = convertBytesToSamples16(audioData);
 
-    // Applying Schroeder reverberation algorithm
-    std::vector<float> reverbSamples = schroederReverb(floatSamples, header.sampleRate);
-    std::vector<float> outputSamplesFloat = mixDryWet(floatSamples, reverbSamples, wetLevel);
+    // Applying pitch shifter algorithm
+    std::vector<float> pitchShiftedSamples;
+    int fftSize = 1024;
+    int hopSize = 256;
 
-    std::vector<uint8_t> outputAudioBytes = convertSamplesToBytes16(outputSamplesFloat);
+    shiftPitch(floatSamples, pitchShiftedSamples, fftSize, hopSize, header.sampleRate, pitchShift);
+
+    // Applying Schroeder reverberation algorithm
+    // std::vector<float> reverbSamples = schroederReverb(floatSamples, header.sampleRate);
+    // std::vector<float> outputSamplesFloat = mixDryWet(floatSamples, reverbSamples, wetLevel);
+
+    // std::vector<uint8_t> outputAudioBytes = convertSamplesToBytes16(outputSamplesFloat);
+    std::vector<uint8_t> outputAudioBytes = convertSamplesToBytes16(pitchShiftedSamples);
 
     saveWavFile(outputFilename, header, outputAudioBytes);
-
+    std::cout <<"Pitch Shifting Done!\n";
     return 0;
 }
