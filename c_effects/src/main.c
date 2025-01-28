047b715d0ae8953cda8da023e94c8f7ba5652526
matsouto <matsouto55@gmail.com>
2025-01-28 17:39:14 -0300

    implement flanger

--- c_effects/src/main.c
+++ c_effects/src/main.c
@@ -1,5 +1,6 @@
 #include "effects/tremolo.h"
 #include "effects/reverb.h"
+#include "effects/flanger.h"
 #include "file.h"
 #include <stdio.h>
 #include <string.h>
@@ -25,6 +26,10 @@ int main()
   int16_t *tremolo_data = (int16_t *)malloc(numSamples * sizeof(int16_t));
   memcpy(tremolo_data, data, numSamples * sizeof(int16_t));
 
+  // copy the data to use in tremolo
+  int16_t *flanger_data = (int16_t *)malloc(numSamples * sizeof(int16_t));
+  memcpy(flanger_data, data, numSamples * sizeof(int16_t));
+
   // Apply the reverb effect
   apply_reverb(reverb_data, numSamples, 0.5f * header.sampleRate, 0.5f);
   const char *reverb_output = "reverb_output.wav";
@@ -38,6 +43,13 @@ int main()
   write_wav(tremolo_output, &header, tremolo_data);
   printf("Tremolo effect applied and saved to %s\n", tremolo_output);
   free(tremolo_data);
+
+  // Apply the tremolo effect
+  apply_flanger(flanger_data, numSamples, header.sampleRate, 5.0f, 2.0f, 0.25f); // Parameters: delay, depth, rate
+  const char *flanger_output = "flanger_output.wav";
+  write_wav(flanger_output, &header, flanger_data);
+  printf("Flanger effect applied and saved to %s\n", flanger_output);
+  free(flanger_data);
 
   // Free allocated memory
   free(data);
